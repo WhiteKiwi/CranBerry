6f12a206b8c40f2932b8af1e2dd60e8a073ed94f
장지훈 <s5v7@outlook.kr>
2019-01-21 09:26:09 +0900

    Question Model 및 Question Manager 추가

---
+++ CranBerry/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CranBerry.Models {
+	/// <summary>
+	/// Question Model
+	/// </summary>
+	public class Question {
+		public int Id { get; set; } // 질문 일련번호
+
+		public string Title { get; set; }   // 질문 제목
+
+		public string Contents { get; set; } // 질문 내용
+
+		public DateTime QuestionAt { get; set; }  // 질문 날짜
+
+		public string UserID { get; set; }  // 등록자 ID
+	}
+}
