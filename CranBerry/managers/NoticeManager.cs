c064807af6148113604be742b3104b4514f24221
장지훈 <s5v7@outlook.kr>
2019-01-21 09:48:04 +0900

    최근 공지들을 가져오는 메서드 추가

--- CranBerry/managers/NoticeManager.cs
+++ CranBerry/managers/NoticeManager.cs
@@ -5,5 +5,37 @@ using System.Collections.Generic;
 
 namespace CranBerry.Managers {
 	public static class NoticeManager {
+		/// <summary>
+		/// Get recent Notices
+		/// </summary>
+		public static List<Notice> GetRecentNotices() {
+			MySqlConnection conn = null;
+			try {
+				// Connect to DB;
+				conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+				conn.Open();
+
+				List<Notice> notices = new List<Notice>();
+
+				string sql = "SELECT * FROM notices ORDER BY Id DESC LIMIT 5;";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+				var rdr = cmd.ExecuteReader();
+				while (rdr.Read()) {
+					notices.Add(new Notice {
+						Id = (int)rdr["Id"],
+						Title = (string)rdr["Title"],
+						Contents = (string)rdr["Contents"],
+						NoticeAt = (DateTime)rdr["Notice_At"]
+					});
+				}
+
+				return notices;
+			} catch (Exception e) {
+				// TODO: 예외 처리
+				throw new Exception(e.Message);
+			} finally {
+				conn.Close();
+			}
+		}
 	}
 }
