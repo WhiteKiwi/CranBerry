7e62d3c9b40d4af306afc971244449d95d3420a3
장지훈 <s5v7@outlook.kr>
2019-01-22 12:17:45 +0900

    QnaAnswer 페이지 이름 변경

---
+++ CranBerry/admin/Question.aspx.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Web.UI;
+
+namespace CranBerry.admin {
+	public partial class Question : System.Web.UI.Page {
+		MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+
+		protected void Page_Lode(object sender, EventArgs e) {
+			if (Request.QueryString["Seq"] == null) {
+				Response.Redirect("QaAList.aspx");
+			} else {
+				if (!Page.IsPostBack) {
+					ReadData();
+				}
+			}
+		}
+
+		private void ReadData() {
+			con.Open();
+			MySqlCommand cmd = new MySqlCommand("question", con);
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.Parameters.AddWithValue("@Num", Request["Seq"]);
+			MySqlDataReader dr = cmd.ExecuteReader();
+			while (dr.Read()) {
+				this.lblTitle1.Text = dr["Title"].ToString();
+				this.lblTitle2.Text = dr["Content"].ToString();
+				this.lblAnswer.Text = dr["Answer"].ToString();
+			}
+			dr.Close();
+			con.Close();
+		}
+
+		protected void btnWrite_Click(object sender, EventArgs e) {
+			string Answer = " ";
+			if (txtAnswer.Text == String.Empty) {
+				string scriptStr = "<script>alert('답변을 입력해주세요.'); history.back() ; </script>";
+				Page.RegisterClientScriptBlock("done", scriptStr);
+			} else {
+				Answer = txtAnswer.Text;
+
+				MySqlCommand cmd = new MySqlCommand();
+				cmd.Connection = con;
+				cmd.CommandText = string.Format("update question set Answer = '{0}' where Number= 2", txtAnswer.Text);
+				con.Open();
+				cmd.ExecuteNonQuery();
+				con.Close();
+				Response.Redirect(Request.RawUrl);
+			}
+		}
+	}
+}
--- CranBerry/admin/Question.aspx.designer.cs
+++ CranBerry/admin/Question.aspx.designer.cs
@@ -7,19 +7,10 @@
 // </자동 생성됨>
 //------------------------------------------------------------------------------
 
-namespace CranBerry {
+namespace CranBerry.admin {
     
     
-    public partial class QnaAnswerr {
-        
-        /// <summary>
-        /// form1 컨트롤입니다.
-        /// </summary>
-        /// <remarks>
-        /// 자동 생성 필드입니다.
-        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    public partial class Question {
         
         /// <summary>
         /// lblTitle1 컨트롤입니다.
