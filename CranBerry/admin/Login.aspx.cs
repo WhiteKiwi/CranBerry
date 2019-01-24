589522710bf8c15621d037fa40bcd6340ed85a6c
abc01055532597@gmail.com <abc01055532597@gmail.com>
2019-01-24 20:35:29 +0900

    로그인기능

--- CranBerry/admin/Login.aspx.cs
+++ CranBerry/admin/Login.aspx.cs
@@ -3,36 +3,49 @@ using System;
 using System.Configuration;
 using System.Data;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
-namespace CranBerry.admin {
-	public partial class Login : System.Web.UI.Page {
-		MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+namespace CranBerry.admin
+{
+    public partial class Login : System.Web.UI.Page
+    {
 
-		protected void BtnLogin_Click(object sender, EventArgs e) {
-			if (UserID.Text == String.Empty) {
-				ScriptManager.RegisterClientScriptBlock(LoginButton, this.GetType(), "AlertMsg", "<script language='javascript'>alert('아이디를 입력해주세요.');</script>", false);
-			}
-			if (UserPW.Text == String.Empty) {
-				ScriptManager.RegisterClientScriptBlock(LoginButton, this.GetType(), "AlertMsg", "<script language='javascript'>alert('비밀번호를 입력해주세요.');</script>", false);
-			}
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
-			con.Open();
-			MySqlCommand cmd = con.CreateCommand();
-			cmd.CommandType = CommandType.Text;
-			cmd.CommandText = "select*from login where ID = '" + UserID.Text + "'and Password='" + UserPW.Text + "'";
-			cmd.ExecuteNonQuery();
-			DataTable dt = new DataTable();
-			MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-			da.Fill(dt);
-			foreach (DataRow dr in dt.Rows) {
-				Session["ID"] = dr["ID"].ToString();
-				Response.Redirect("AdminQaA.aspx");
-			}
+            Button ButtonLogin = new Button();
+            ButtonLogin.ID = "btnClick";
 
-			con.Close();
-			ScriptManager.RegisterClientScriptBlock(LoginButton, this.GetType(), "AlertMsg", "<script language='javascript'>alert('회원정보가 일치하지 않습니다.');</script>", false);
 
-		}
-	}
+        }
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            //계정 확인
+            TextBox UserID = new TextBox();
+            TextBox UserPW = new TextBox();
+
+            MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+            con.Open();
+            MySqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "select*from login where ID = '" + UserID.Text + "' and Password='" + UserPW.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                Session["UserID"] = dr["UserID"].ToString();
+                Response.Redirect("Default.aspx");
+            }
+            Page.ClientScript.RegisterClientScriptBlock(typeof(Page), "Alert", "alert('회원정보가 일치하지않습니다.')", true);
+
+        }
+
+
+    }
+
+
 }
+
+
 
