6c2724e5c32d8c97f60ee8e940c3dbb257b58aac
정민 서 <jeongmin0518@naver.com>
2019-01-28 20:11:31 +0900

    계정추가 메서드

--- CranBerry/managers/AdminManager.cs
+++ CranBerry/managers/AdminManager.cs
@@ -5,35 +5,62 @@ using System.Collections.Generic;
 
 namespace CranBerry.Managers {
 	public static class AdminManager {
-		/// <summary>
-		/// Get Admin List
-		/// </summary>
-		public static List<Admin> GetAdmins() {
-			MySqlConnection conn = null;
-			try {
-				// Connect to DB;
-				conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
-				conn.Open();
+        /// <summary>
+        /// Get Admin List
+        /// </summary>
+        public static List<Admin> GetAdmins() {
+            MySqlConnection conn = null;
+            try {
+                // Connect to DB;
+                conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+                conn.Open();
 
-				List<Admin> admins = new List<Admin>();
+                List<Admin> admins = new List<Admin>();
 
-				string sql = "SELECT * FROM admins;";
-				MySqlCommand cmd = new MySqlCommand(sql, conn);
-				var rdr = cmd.ExecuteReader();
-				while (rdr.Read()) {
-					admins.Add(new Admin {
-						ID = (string)rdr["Id"],
-						Name = (string)rdr["Name"]
-					});
-				}
+                string sql = "SELECT * FROM admins;";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                var rdr = cmd.ExecuteReader();
+                while (rdr.Read()) {
+                    admins.Add(new Admin {
+                        ID = (string)rdr["Id"],
+                        Name = (string)rdr["Name"]
+                    });
+                }
 
-				return admins;
-			} catch (Exception e) {
-				// TODO: 예외 처리
-				throw new Exception(e.Message);
-			} finally {
-				conn.Close();
-			}
-		}
-	}
-}
+                return admins;
+            } catch (Exception e) {
+                // TODO: 예외 처리
+                throw new Exception(e.Message);
+            } finally {
+                conn.Close();
+            }
+        }
+
+        //계정추가
+        public static int AddAdmin(Admin admin)
+            {
+                int result = 0;
+                MySqlConnection con = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = string.Format("insert into admins(Id, Name)values('{0}','{1}')", admin.ID, admin.Name);
+
+                con.Open();
+                result = cmd.ExecuteNonQuery();
+                return result;
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        }
+    }
