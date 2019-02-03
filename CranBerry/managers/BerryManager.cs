6552f3d7baed5d2c4f7c358e1cdcf75efaacaa75
abc01055532597@gmail.com <abc01055532597@gmail.com>
2019-02-03 20:28:17 +0900

    BerryManger/AddBerry 추가

--- CranBerry/managers/BerryManager.cs
+++ CranBerry/managers/BerryManager.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using CranBerry.Models;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
 namespace CranBerry.managers
 {
-    public class BerryManager
+    public static class BerryManager //Beryy 추가
     {
+        public static int AddBerry(Models.Berry berry)
+        {
+
+            int result = 0;
+            MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = string.Format("insert into berries(Title,Contents,Classification)values('{0}','{1}','{2}')", berry.Title, berry.Contents, berry.Classification);
+                con.Open();
+                result = cmd.ExecuteNonQuery();
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
     }
+
+
 }
+
