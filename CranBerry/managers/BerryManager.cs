e71fe68b064283664b55f8eec7aab948ebadd0a9
abc01055532597@gmail.com <abc01055532597@gmail.com>
2019-02-03 20:57:11 +0900

    BerryManager/DeleteBerry 베리삭제 추가

--- CranBerry/managers/BerryManager.cs
+++ CranBerry/managers/BerryManager.cs
@@ -58,7 +58,27 @@ namespace CranBerry.managers
         {
             con.Close();
         }
-         }
+      }
+    public static int DeleteBerry(Berry berry) // 베리삭제
+        {
+            MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+            MySqlCommand cmd = new MySqlCommand();
+           try{
+                int result = 0;
+                cmd.Connection = con;
+                cmd.CommandText = string.Format("Delete From berries Where Id={0}", berry.Id);
+                con.Open();
+                result = cmd.ExecuteNonQuery();
+                return result;
+
+            }catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally{
+                con.Close();
+            }
+        }
     }
 
 }
