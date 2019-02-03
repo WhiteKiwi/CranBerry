5a23c6a18abfa9d502c3497d9c9bcb12a73c153d
abc01055532597@gmail.com <abc01055532597@gmail.com>
2019-02-03 20:16:01 +0900

    Model/Classification.cs 추가

---
+++ CranBerry/Models/Classification.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CranBerry.Models
+{
+    public enum Classification
+    {
+        // 행사
+        Event,
+
+        // 학교시설
+        Facilities,
+
+        // CNSA 용어
+        CNSATerms,
+
+        // 생활
+        CNSALife,
+
+        // 학습
+        Study,
+
+        // 동아리
+        Club,
+
+        // 단체
+        Group,
+
+        // 대회
+        Contest
+    }
+}
