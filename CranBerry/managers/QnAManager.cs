456d05df6a2bcccb9b7b5271c4314ff4cb9d6082
장지훈 <s5v7@outlook.kr>
2019-02-04 22:37:22 +0900

    페이지, 검색 옵션에 따른 질문 목록 가져오는 함수 추가

--- CranBerry/managers/QnAManager.cs
+++ CranBerry/managers/QnAManager.cs
@@ -4,249 +4,301 @@ using System.Collections.Generic;
 using System.Configuration;
 
 namespace CranBerry.Managers {
-    public static class QnAManager
-    {
-        /// <summary>
-        /// Get recent QnAs
-        /// </summary>
-        public static List<Models.Question> GetRecentQuestions()
-        {
-            MySqlConnection conn = null;
-            try
-            {
-                // Connect to DB;
-                conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
-                conn.Open();
+	public static class QnAManager {
+		/// <summary>
+		/// Get recent QnAs
+		/// </summary>
+		public static List<Models.Question> GetRecentQuestions() {
+			MySqlConnection conn = null;
+			try {
+				// Connect to DB;
+				conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+				conn.Open();
 
-                List<Models.Question> questions = new List<Models.Question>();
+				List<Models.Question> questions = new List<Models.Question>();
 
-                string sql = "SELECT * FROM questions ORDER BY Id DESC LIMIT 9;";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                var rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    questions.Add(new Models.Question
-                    {
-                        Id = (int)rdr["Id"],
-                        Title = (string)rdr["Title"],
-                        Contents = (string)rdr["Contents"],
-                        QuestionAt = (DateTime)rdr["Question_At"]
-                    });
-                }
+				string sql = "SELECT * FROM questions ORDER BY Id DESC LIMIT 9;";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+				var rdr = cmd.ExecuteReader();
+				while (rdr.Read()) {
+					questions.Add(new Models.Question {
+						Id = (int)rdr["Id"],
+						Title = (string)rdr["Title"],
+						Contents = (string)rdr["Contents"],
+						QuestionAt = (DateTime)rdr["Question_At"]
+					});
+				}
 
-                return questions;
-            }
-            catch (Exception e)
-            {
-                // TODO: 예외 처리
-                throw new Exception(e.Message);
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
+				return questions;
+			} catch (Exception e) {
+				// TODO: 예외 처리
+				throw new Exception(e.Message);
+			} finally {
+				conn.Close();
+			}
+		}
 
-        /// <summary>
-        /// Get QnA by Id
-        /// </summary>
-        public static Models.Question GetQnA(int Id)
-        {
-            MySqlConnection conn = null;
-            try
-            {
-                // Connect to DB;
-                conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
-                conn.Open();
+		/// <summary>
+		/// Get QnA by Id
+		/// </summary>
+		public static Models.Question GetQnA(int Id) {
+			MySqlConnection conn = null;
+			try {
+				// Connect to DB;
+				conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+				conn.Open();
 
-                string sql = "SELECT * FROM questions WHERE Id=" + Id + ";";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                var rdr = cmd.ExecuteReader();
-                rdr.Read();
-                Models.Question question = new Models.Question
-                {
-                    Id = (int)rdr["Id"],
-                    Title = (string)rdr["Title"],
-                    Contents = (string)rdr["Contents"],
-                    QuestionAt = (DateTime)rdr["Question_At"],
-                    Answer = (string)rdr["Answer"]
-                };
-                rdr.Close();
+				string sql = "SELECT * FROM questions WHERE Id=" + Id + ";";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+				var rdr = cmd.ExecuteReader();
+				rdr.Read();
+				Models.Question question = new Models.Question {
+					Id = (int)rdr["Id"],
+					Title = (string)rdr["Title"],
+					Contents = (string)rdr["Contents"],
+					QuestionAt = (DateTime)rdr["Question_At"],
+					Answer = (string)rdr["Answer"]
+				};
+				rdr.Close();
 
-                return question;
-            }
-            catch (Exception e)
-            {
-                // TODO: 예외 처리
-                throw new Exception(e.Message);
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
-        /*
+				return question;
+			} catch (Exception e) {
+				// TODO: 예외 처리
+				throw new Exception(e.Message);
+			} finally {
+				conn.Close();
+			}
+		}
+		/*
          * 이준모 - 랜덤 문자열 생성
          */
-        public static char[] rndchrset = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-        //사용될 문자들
+		public static char[] rndchrset = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+		//사용될 문자들
 
-        public static string RandText()
-        {
-            Random rnd = new Random();
-            int i;
-            string randstr = "";
-            for (i = 0; i < 7; i++) randstr += rndchrset[rnd.Next(0, 61)];
-            return randstr;
-        }
+		public static string RandText() {
+			Random rnd = new Random();
+			int i;
+			string randstr = "";
+			for (i = 0; i < 7; i++) randstr += rndchrset[rnd.Next(0, 61)];
+			return randstr;
+		}
 
-        /*
+		/*
          * 서정민 - 페이지 질문 불러오기
          */
 
-        public static List<Models.Question> GetQuestionsByPage(int page)
-        {
-            MySqlConnection conn = null;
-            try
-            {
-                // Connect to DB;
-                conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
-                conn.Open();
+		public static List<Models.Question> GetQuestionsByPage(int page) {
+			MySqlConnection conn = null;
+			try {
+				// Connect to DB;
+				conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+				conn.Open();
 
-                List<Models.Question> questions = new List<Models.Question>();
+				List<Models.Question> questions = new List<Models.Question>();
 
-                // Get Questions Count
-                string sql = "SELECT count(*) FROM questions;";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                int questionCount = Convert.ToInt32(cmd.ExecuteScalar());
+				// Get Questions Count
+				string sql = "SELECT count(*) FROM questions;";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+				int questionCount = Convert.ToInt32(cmd.ExecuteScalar());
 
-                // Get Questions
-                sql = "SELECT Id, Title, Question_At FROM questions ORDER BY Id DESC LIMIT 10 OFFSET " + ((page - 1) * 10) + ";";
-                cmd.CommandText = sql;
+				// Get Questions
+				sql = "SELECT Id, Title, Question_At FROM questions ORDER BY Id DESC LIMIT 10 OFFSET " + ((page - 1) * 10) + ";";
+				cmd.CommandText = sql;
 
-                var rdr = cmd.ExecuteReader();
-                rdr.Read();
-                while (rdr.Read())
-                {
-                    questions.Add(new Models.Question
-                    {
-                        Id = (int)rdr["Id"],
-                        Title = (string)rdr["Title"],
-                        QuestionAt = (DateTime)rdr["Question_At"]
-                    });
-                }
+				var rdr = cmd.ExecuteReader();
+				rdr.Read();
+				while (rdr.Read()) {
+					questions.Add(new Models.Question {
+						Id = (int)rdr["Id"],
+						Title = (string)rdr["Title"],
+						QuestionAt = (DateTime)rdr["Question_At"]
+					});
+				}
 
-                return questions;
-            }
-            catch (Exception e)
-            {
-                // TODO: 예외 처리
-                throw new Exception(e.Message);
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
-        //질문 수정
-        public static int ModifyQuestion(Models.Question question, string RandText)
-        {
-            MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
-            MySqlCommand cmd = new MySqlCommand();
+				return questions;
+			} catch (Exception e) {
+				// TODO: 예외 처리
+				throw new Exception(e.Message);
+			} finally {
+				conn.Close();
+			}
+		}
+		//질문 수정
+		public static int ModifyQuestion(Models.Question question, string RandText) {
+			MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+			MySqlCommand cmd = new MySqlCommand();
 
-            if (question.Answer != "0" )
-                return -1;
-                    
-            {
-                try
-                {
-                    int result = 0;
-                    
-                    cmd.Connection = con;
-                    cmd.CommandText = string.Format("SELECT RandText FROM questions WHERE Id = "+ question.Id);
-                    con.Open();
-                    if (RandText == (string)cmd.ExecuteScalar())
-                    {
-                        //질문 업데이트
-                        cmd.CommandText = string.Format("update question set Contents='{0}' where Id='{1}'", question.Contents, question.Id);
-                        result = cmd.ExecuteNonQuery();
-                        return result;
-                    }
-                    else return -1;
-                }
-                catch (Exception e)
-                {
-                    throw new Exception(e.Message);
-                }
-                finally
-                {
-                    con.Close();
-                }
+			if (question.Answer != "0")
+				return -1;
 
-            }
-        }
-        public static int AddAnswer(Models.Question answer)
-        {
-            int result = 0;
-            MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
-            MySqlCommand cmd = new MySqlCommand();
-            try
-            {
+			{
+				try {
+					int result = 0;
 
-               
-                con.Open();
-                cmd.Connection = con;
-                cmd.CommandText = string.Format("update answer Set Answer=@Answer, Id= @Id where Id=" + answer.Id);
+					cmd.Connection = con;
+					cmd.CommandText = string.Format("SELECT RandText FROM questions WHERE Id = " + question.Id);
+					con.Open();
+					if (RandText == (string)cmd.ExecuteScalar()) {
+						//질문 업데이트
+						cmd.CommandText = string.Format("update question set Contents='{0}' where Id='{1}'", question.Contents, question.Id);
+						result = cmd.ExecuteNonQuery();
+						return result;
+					} else return -1;
+				} catch (Exception e) {
+					throw new Exception(e.Message);
+				} finally {
+					con.Close();
+				}
 
-                cmd.Parameters.AddWithValue("@Answer", answer.Contents);
-                cmd.Parameters.AddWithValue("@Id", answer.Id);
-                result = cmd.ExecuteNonQuery();
-                return result;
-              
-                
+			}
+		}
+		public static int AddAnswer(Models.Question answer) {
+			int result = 0;
+			MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+			MySqlCommand cmd = new MySqlCommand();
+			try {
+				con.Open();
+				cmd.Connection = con;
+				cmd.CommandText = string.Format("update answer Set Answer=@Answer, Id= @Id where Id=" + answer.Id);
 
-            }
-            catch(Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-            finally
-            {
-                cmd.Dispose();
-                con.Close();
-            }
+				cmd.Parameters.AddWithValue("@Answer", answer.Contents);
+				cmd.Parameters.AddWithValue("@Id", answer.Id);
+				result = cmd.ExecuteNonQuery();
+				return result;
+			} catch (Exception e) {
+				throw new Exception(e.Message);
+			} finally {
+				cmd.Dispose();
+				con.Close();
+			}
+		}
 
+		public static void DeleteQuestion(int id) {
+			MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+			MySqlCommand cmd = new MySqlCommand();
+			try {
+				con.Open();
+				cmd.Connection = con;
+				cmd.CommandText = string.Format("DELETE FROM question WHERE Id=" + id);
+			} catch (Exception e) {
+				throw new Exception(e.Message);
+			} finally {
 
-            }
+				con.Close();
+			}
+		}
 
-        public static void DeleteQuestion(int id)
-        {
-            MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
-            MySqlCommand cmd = new MySqlCommand();
-            try
-            {
-                con.Open();
-                cmd.Connection = con;
-                cmd.CommandText = string.Format("DELETE FROM question WHERE Id=" + id);
-            }
-            catch(Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-            finally
-            {
-                
-                con.Close();
-            }
+		/// <summary>
+		/// Get Pages Count
+		/// </summary>
+		public static int GetPagesCount() {
+			MySqlConnection conn = null;
+			try {
+				// Connect to DB;
+				conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+				conn.Open();
 
+				// Get Questions Count
+				string sql = "SELECT count(*) FROM questions;";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+				int questionCount = Convert.ToInt32(cmd.ExecuteScalar());
 
-        }
+				// 공지 갯수의 1의 자리가 0일 경우
+				if (questionCount % 10 != 0) {
+					return questionCount / 10 + 1;
+				} else {
+					return questionCount / 10;
+				}
+			} catch (Exception e) {
+				// TODO: 예외 처리
+				throw new Exception(e.Message);
+			} finally {
+				conn.Close();
+			}
+		}
 
-        }
+		/// <summary>
+		/// Get Pages Count
+		/// </summary>
+		public static int GetPagesCountBySearching(string text, bool isContents) {
+			MySqlConnection conn = null;
+			try {
+				// Connect to DB;
+				conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+				conn.Open();
 
+				// SetColumName
+				string columName = "";
+				if (isContents)
+					columName = "Contents";
+				else
+					columName = "Title";
 
-        
-        
-   }
+				// Get Questions Count
+				string sql = "SELECT count(*) FROM questions WHERE " + columName + " LIKE '%" + text + "%';";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+				int questionCount = Convert.ToInt32(cmd.ExecuteScalar());
+
+				// 공지 갯수의 1의 자리가 0일 경우
+				if (questionCount % 10 != 0) {
+					return questionCount / 10 + 1;
+				} else {
+					return questionCount / 10;
+				}
+			} catch (Exception e) {
+				// TODO: 예외 처리
+				throw new Exception(e.Message);
+			} finally {
+				conn.Close();
+			}
+		}
+
+		/// Get Questions by Searching
+		/// 목록 렌더링을 위해 제목과 글번호만 반환
+		/// </summary>
+		public static List<Models.Question> GetQuestionsBySearching(int page, string text, bool isContents) {
+			MySqlConnection conn = null;
+			try {
+				// Connect to DB;
+				conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
+				conn.Open();
+
+				List<Models.Question> questionList = new List<Models.Question>();
+
+				// SetColumName
+				string columName = "";
+				if (isContents)
+					columName = "Contents";
+				else
+					columName = "Title";
+
+				// Get Questions Count
+				string sql = "SELECT count(*) FROM questions WHERE " + columName + " LIKE '%" + text + "%';";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+				int questionCount = Convert.ToInt32(cmd.ExecuteScalar());
+
+				// Get Questions
+				sql = "SELECT Id, Title, Question_At FROM questions WHERE " + columName + " LIKE '%" + text + "%' ORDER BY Id DESC LIMIT 10 OFFSET " + ((page - 1) * 10) + ";";
+				cmd.CommandText = sql;
+
+				var rdr = cmd.ExecuteReader();
+				while (rdr.Read()) {
+					questionList.Add(new Models.Question {
+						Id = (int)rdr["Id"],
+						Title = (string)rdr["Title"],
+						QuestionAt = (DateTime)rdr["Question_At"]
+					});
+				}
+
+				return questionList;
+			} catch (Exception e) {
+				// TODO: 예외 처리
+				throw new Exception(e.Message);
+			} finally {
+				conn.Close();
+			}
+		}
+	}
+}
