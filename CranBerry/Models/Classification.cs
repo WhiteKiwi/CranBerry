b821e8e42b62548c6ce1f02a10af125c7fde7239
장지훈 <s5v7@outlook.kr>
2019-02-04 10:14:02 +0900

    Berry 분류 업데이트

--- CranBerry/Models/Classification.cs
+++ CranBerry/Models/Classification.cs
@@ -1,34 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿namespace CranBerry.Models {
+	/// <summary>
+	/// 분류 enum
+	/// </summary>
+	public enum Classification {
+		// 행사
+		Event,
 
-namespace CranBerry.Models
-{
-    public enum Classification
-    {
-        // 행사
-        Event,
+		// 학교시설
+		Facilities,
 
-        // 학교시설
-        Facilities,
+		// CNSA 용어
+		CNSATerms,
 
-        // CNSA 용어
-        CNSATerms,
+		// 생활
+		CNSALifeAndStudy,
 
-        // 생활
-        CNSALife,
+		// 학습
+		Dormitory,
 
-        // 학습
-        Study,
+		// 동아리
+		Club,
 
-        // 동아리
-        Club,
+		// 단체
+		Group,
 
-        // 단체
-        Group,
-
-        // 대회
-        Contest
-    }
+		// 대회
+		Contest
+	}
 }
