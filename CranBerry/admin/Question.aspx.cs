c1b9e69d80306fc444486a7825b77302b81b7228
abc01055532597@gmail.com <abc01055532597@gmail.com>
2019-02-04 14:58:44 +0900

    question.aspx.cs에 있는 코드 삭제

--- CranBerry/admin/Question.aspx.cs
+++ CranBerry/admin/Question.aspx.cs
@@ -6,48 +6,6 @@ using System.Web.UI;
 
 namespace CranBerry.admin {
 	public partial class Question : System.Web.UI.Page {
-		MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
-
-		protected void Page_Load(object sender, EventArgs e) {
-			if (Request.QueryString["Seq"] == null) {
-				Response.Redirect("Questions.aspx");
-			} else {
-				if (!Page.IsPostBack) {
-					ReadData();
-				}
-			}
-		}
-
-		private void ReadData() {
-			con.Open();
-			MySqlCommand cmd = new MySqlCommand("question", con);
-			cmd.CommandType = CommandType.StoredProcedure;
-			cmd.Parameters.AddWithValue("@Num", Request["Seq"]);
-			MySqlDataReader dr = cmd.ExecuteReader();
-			while (dr.Read()) {
-				this.lblTitle1.Text = dr["Title"].ToString();
-				this.lblTitle2.Text = dr["Content"].ToString();
-				this.lblAnswer.Text = dr["Answer"].ToString();
-			}
-			dr.Close();
-			con.Close();
-		}
-
-		protected void btnWrite_Click(object sender, EventArgs e) {
-			string Answer = " ";
-			if (txtAnswer.Text == String.Empty) {
-                ClientScript.RegisterStartupScript(this.GetType(), "enterAnswer", "alert('답변을 입력해주세요.')", true);
-            } else {
-				Answer = txtAnswer.Text;
-
-				MySqlCommand cmd = new MySqlCommand();
-				cmd.Connection = con;
-				cmd.CommandText = string.Format("update question set Answer = '{0}' where Number= 2", txtAnswer.Text);
-				con.Open();
-				cmd.ExecuteNonQuery();
-				con.Close();
-				Response.Redirect(Request.RawUrl);
-			}
-		}
+		
 	}
 }
