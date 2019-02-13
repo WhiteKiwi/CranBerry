4dbb1cd7c69cc7541673e91d7e8357a45002812f
정민 서 <jeongmin0518@naver.com>
2019-02-13 22:14:05 +0900

    UploadBerry.aspx.cs 코드 추가

--- CranBerry/admin/UploadBerry.aspx.cs
+++ CranBerry/admin/UploadBerry.aspx.cs
@@ -5,10 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace CranBerry.admin {
-	public partial class UploadBerry : System.Web.UI.Page {
-		protected void Page_Load(object sender, EventArgs e) {
+namespace CranBerry.admin
+{
+    public partial class UploadBerry : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
-		}
-	}
+        }
+
+        void UploadBtn_Click(object sender, EventArgs e)
+        {
+            Managers.BerryManager.AddBerry(new Models.Berry
+            {
+                Title = BerryTitle.Text,
+                Contents = BerryContent.Text
+            });
+        }
+    }
 }
