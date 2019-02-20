de3b5cef54444dd762624afd11617c788a549c29
정민 서 <jeongmin0518@naver.com>
2019-02-20 21:09:00 +0900

    EditNotice 페이지 완성

--- CranBerry/admin/EditNotice.aspx.cs
+++ CranBerry/admin/EditNotice.aspx.cs
@@ -8,7 +8,23 @@ using System.Web.UI.WebControls;
 namespace CranBerry.admin {
 	public partial class EditNotice : System.Web.UI.Page {
 		protected void Page_Load(object sender, EventArgs e) {
-           
+            var noticeID = Request.QueryString["id"];
+
+            var notice = Managers.NoticeManager.GetNoticeByID(int.Parse(noticeID));
+
+            TitleLabel.Text = notice.Title;
+            ContentsLabel.Text = notice.Contents;
         }
-	}
+
+        protected void NoticeSaveButton_Click(object sender, EventArgs e)
+        {
+            Managers.NoticeManager.ModifyNotice(new Models.Notice
+            {
+                Id = int.Parse(Request.QueryString["id"]),
+                Contents = ContentsLabel.Text
+
+            });
+        }
+
+    }
 }
