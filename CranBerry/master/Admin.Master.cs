01e27a50e4826bc56638c395691ba38ab05fa98e
병준 김 <q1q5q9@naver.com>
2019-02-25 23:10:35 +0900

    관리자 페이지 Navbar 추가

--- CranBerry/master/Admin.Master.cs
+++ CranBerry/master/Admin.Master.cs
@@ -4,14 +4,14 @@ namespace CranBerry.master
 {
     public partial class Admin : System.Web.UI.MasterPage
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            // 세션이 비어 있을 경우
-            if (Session["AdminID"] == null)
-            {
-                // 로그인 페이지로 Redirect
-                Response.Redirect("/admin/Login.aspx");
-            }
-        }
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+        //    // 세션이 비어 있을 경우
+        //    if (Session["AdminID"] == null)
+        //    {
+        //        // 로그인 페이지로 Redirect
+        //        Response.Redirect("/admin/Login.aspx");
+        //    }
+        //}
     }
 }
