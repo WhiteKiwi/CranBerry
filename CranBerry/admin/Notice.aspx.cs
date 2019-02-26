a75b967222b3063d4fa8f75837a2f131c84065e0
정민 서 <jeongmin0518@naver.com>
2019-02-26 17:45:28 +0900

    notice 페이지 완성

--- CranBerry/admin/Notice.aspx.cs
+++ CranBerry/admin/Notice.aspx.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 
+namespace CranBerry.admin
+{
+    public partial class Notice : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var noticeID = Request.QueryString["id"];
 
-namespace CranBerry.admin {
-	public partial class Notice : System.Web.UI.Page {
-		
+            // noticeid가 존재할 경우
+            if (!String.IsNullOrEmpty(noticeID))
+            {
+                var notice = Managers.NoticeManager.GetNoticeByID(int.Parse(noticeID));
 
+                nTitle.Text = notice.Title;
+                Contents.Text = notice.Contents;
+                NoticeAt.Text = notice.NoticeAt.ToString("yyyy-MM-dd");
+            }
+            else
+            {
+                nTitle.Text = "404 Not Found.";
+                Contents.Text = "404 Not Found.";
+                NoticeAt.Text = "404 Not Found.";
+            }
+        }
 
+        protected void DeleteButton_Click(object sender, EventArgs e)
+        {
+            var noticeID = Request.QueryString["id"];
+
+            Managers.NoticeManager.DeleteNotice(new Models.Notice
+            {
+                id = int.Parse(noticeID)
+            });
+        }
     }
 }
