bb43ffd9ef820aceee363cd1102d826c97796503
정민 서 <jeongmin0518@naver.com>
2019-02-26 17:34:30 +0900

    uploadnotice 페이지 완성

--- CranBerry/admin/UploadNotice.aspx.cs
+++ CranBerry/admin/UploadNotice.aspx.cs
@@ -12,7 +12,14 @@ namespace CranBerry.admin
 {
     public partial class UploadNotice : System.Web.UI.Page
     {
-       
+       protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            Managers.NoticeManager.ModifyNotice(new Models.Notice
+            {
+                Title = TitleBox.Text,
+                Contents = ContentsBox.Text
+            });
+        }
         
 
     }
