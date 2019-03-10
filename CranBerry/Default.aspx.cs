ff2d9dd37c689a91b20c78d9f4d84b735e508ebf
병준 김 <q1q5q9@naver.com>
2019-03-10 13:27:33 +0900

    날짜 조회수

--- CranBerry/Default.aspx.cs
+++ CranBerry/Default.aspx.cs
@@ -10,14 +10,30 @@ using System.Web.UI.WebControls;
 
 namespace CranBerry {
 	public partial class Default : System.Web.UI.Page {
-		protected void Page_Load(object sender, EventArgs e) {
+        protected void Page_Load(object sender, EventArgs e) {
+            var Time;
             MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["CranBerry"].ConnectionString);
             conn.Open();
-            MySqlCommand cmdd = new MySqlCommand("Update User Set Visit = Visit + 1 where Total="+ "Total", conn);
+            MySqlCommand cmdd = new MySqlCommand("Update User Set Visit = Visit + 1 where Total=" + "Total", conn);
             cmdd.CommandType = CommandType.Text;
             cmdd.ExecuteNonQuery();
             cmdd.Dispose();
             conn.Close();
+            if (Db에 날짜가 있으면){
+
+                cmdd = new MySqlCommand("Update User Set Visit = Visit + 1 where Total=" + DateTime.Now, conn);
+                cmdd.CommandType = CommandType.Text;
+                cmdd.ExecuteNonQuery();
+                cmdd.Dispose();
+                conn.Close();
+            }
+            else{
+                cmdd = new MySqlCommand("Insert User Set Visit = 1 and Total =" + DateTime.Now);
+                cmdd.CommandType = CommandType.Text;
+                cmdd.ExecuteNonQuery();
+                cmdd.Dispose();
+                conn.Close();
+            }
         }
 	}
 }   
