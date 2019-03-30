cc4dcd3c83ce5a686b30588acb5e3a63b1fea498
병준 김 <q1q5q9@naver.com>
2019-03-30 10:33:16 +0900

    달력 주석

--- CranBerry/admin/Calendar.aspx.cs
+++ CranBerry/admin/Calendar.aspx.cs
@@ -15,54 +15,54 @@ namespace CranBerry.admin
 {
     public partial class Calendar : System.Web.UI.Page
     {
-        Hashtable HolidayList;
+        //Hashtable HolidayList;
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            HolidayList = Getholiday();
-            Calendar1.Caption = "Calender - Author: Puran Singh Mehra";
-            Calendar1.FirstDayOfWeek = FirstDayOfWeek.Sunday;
-            Calendar1.NextPrevFormat = NextPrevFormat.ShortMonth;
-            Calendar1.TitleFormat = TitleFormat.Month;
-            Calendar1.ShowGridLines = true;
-            Calendar1.DayStyle.Height = new Unit(50);
-            Calendar1.DayStyle.Width = new Unit(150);
-            Calendar1.DayStyle.HorizontalAlign = HorizontalAlign.Center;
-            Calendar1.DayStyle.VerticalAlign = VerticalAlign.Middle;
-            Calendar1.OtherMonthDayStyle.BackColor = System.Drawing.Color.AliceBlue;
-        }
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+        //    HolidayList = Getholiday();
+        //    Calendar1.Caption = "Calender - Author: Puran Singh Mehra";
+        //    Calendar1.FirstDayOfWeek = FirstDayOfWeek.Sunday;
+        //    Calendar1.NextPrevFormat = NextPrevFormat.ShortMonth;
+        //    Calendar1.TitleFormat = TitleFormat.Month;
+        //    Calendar1.ShowGridLines = true;
+        //    Calendar1.DayStyle.Height = new Unit(50);
+        //    Calendar1.DayStyle.Width = new Unit(150);
+        //    Calendar1.DayStyle.HorizontalAlign = HorizontalAlign.Center;
+        //    Calendar1.DayStyle.VerticalAlign = VerticalAlign.Middle;
+        //    Calendar1.OtherMonthDayStyle.BackColor = System.Drawing.Color.AliceBlue;
+        //}
 
-        private Hashtable Getholiday()
-        {
-            Hashtable holiday = new Hashtable();
-            holiday["1/1/2019"] = "New Year";
-            holiday["1/1/2019"] = "Guru Govind Singh Jayanti";
+        //private Hashtable Getholiday()
+        //{
+        //    Hashtable holiday = new Hashtable();
+        //    holiday["1/1/2019"] = "New Year";
+        //    holiday["1/1/2019"] = "Guru Govind Singh Jayanti";
 
-            return holiday;
-        }
+        //    return holiday;
+        //}
 
-        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
-        {
-            LabelAction.Text = "Date changed to :" + Calendar1.SelectedDate.ToShortDateString();
-        }
+        //protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        //{
+        //    LabelAction.Text = "Date changed to :" + Calendar1.SelectedDate.ToShortDateString();
+        //}
 
-        protected void Calendar1_VisibleMonthChanged(object sender, MonthChangedEventArgs e)
-        {
-            LabelAction.Text = "Month changed to :" + e.NewDate.ToShortDateString();
-        }
+        //protected void Calendar1_VisibleMonthChanged(object sender, MonthChangedEventArgs e)
+        //{
+        //    LabelAction.Text = "Month changed to :" + e.NewDate.ToShortDateString();
+        //}
 
-        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
-        {
-            if (HolidayList[e.Day.Date.ToShortDateString()] != null)
-            {
-                Literal literal1 = new Literal();
-                literal1.Text = "<br/>";
-                e.Cell.Controls.Add(literal1);
-                Label label1 = new Label();
-                label1.Text = (string)HolidayList[e.Day.Date.ToShortDateString()];
-                label1.Font.Size = new FontUnit(FontSize.Small);
-                e.Cell.Controls.Add(label1);
-            }
-        }
+        //protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        //{
+        //    if (HolidayList[e.Day.Date.ToShortDateString()] != null)
+        //    {
+        //        Literal literal1 = new Literal();
+        //        literal1.Text = "<br/>";
+        //        e.Cell.Controls.Add(literal1);
+        //        Label label1 = new Label();
+        //        label1.Text = (string)HolidayList[e.Day.Date.ToShortDateString()];
+        //        label1.Font.Size = new FontUnit(FontSize.Small);
+        //        e.Cell.Controls.Add(label1);
+        //    }
+        //}
     }
 }
